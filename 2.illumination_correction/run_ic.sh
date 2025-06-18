95ac1967dcc5e1649a5ac84fbaf830494f0d2fb3
MikeLippincott <1michaell2017@gmail.com>
2025-06-18 13:33:07 -0600

    IC

--- 2.illumination_correction/run_ic.sh
+++ 2.illumination_correction/run_ic.sh
@@ -6,7 +6,16 @@ conda init bash
 conda activate gff_cp_env
 
 # convert Jupyter notebooks to scripts
-jupyter nbconvert --to script --output-dir=scripts/ *.ipynb
+jupyter nbconvert --to script --output-dir=scripts/ notebooks/*.ipynb
+
+
+cd scripts/ || exit 1
 
 # run Python script for performing illumination correction with CellProfiler
-python scripts/cp_illum_correction.py
+python cp_illum_correction.py
+
+conda deactivate
+
+cd ../ || exit 1
+
+echo "Illumination correction completed successfully."
