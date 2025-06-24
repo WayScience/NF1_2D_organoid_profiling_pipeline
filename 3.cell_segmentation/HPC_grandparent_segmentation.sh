01c4db5b62bb2a91c3a7a91e1e2cc399f240b008
MikeLippincott <1michaell2017@gmail.com>
2025-06-24 09:57:30 -0600

    fixing slurm scripts

--- 3.cell_segmentation/HPC_grandparent_segmentation.sh
+++ 3.cell_segmentation/HPC_grandparent_segmentation.sh
@@ -4,20 +4,14 @@
 #SBATCH --account=amc-general
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
-#SBATCH --time=00:10:00
+#SBATCH --time=00:30:00
 #SBATCH --output="segmentation_grandparent-%j.out"
 
-
-module load anaconda
-# initialize the correct shell for your machine to allow conda to work (see README for note on shell names)
-conda init bash
-# activate the preprocessing environment
-conda activate GFF_segmentation
+jupyter nbconvert --to script --output-dir=scripts/ notebooks/*.ipynb
 
 patients_file="../data/patient_IDs.txt"
 # read patient IDs from the file
 mapfile -t patients < "$patients_file"
-
 
 for patient in "${patients[@]}"; do
     number_of_jobs=$(squeue -u $USER | wc -l)
@@ -29,15 +23,12 @@ for patient in "${patients[@]}"; do
         --nodes=1 \
         --ntasks=1 \
         --account=amc-general \
-        --parition=amilan \
+        --partition=amilan \
         --qos=normal \
         --time=00:10:00 \
         --output="segmentation_parent_${patient}-%j.out" \
         HPC_parent_segmentation.sh \
         "$patient"
 done
-
-
-conda deactivate
 
 echo "Cell segmentation preprocessing completed successfully."
--- 3.cell_segmentation/HPC_parent_segmentation.sh
+++ 3.cell_segmentation/HPC_parent_segmentation.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-module load anaconda
-# initialize the correct shell for your machine to allow conda to work (see README for note on shell names)
-conda init bash
-# activate the preprocessing environment
-conda activate GFF_segmentation
-
-cd scripts/ || exit
-
 patient=$1
-z_stack_dir="../../data/$patient/zstack_images/"
+z_stack_dir="../data/$patient/zstack_images/"
 
 mapfile -t well_fovs < <(ls -d "$z_stack_dir"/*)
 
 total_dirs=$(echo "${well_fovs[@]}" | wc -w)
 echo "Total directories: $total_dirs"
-twoD_methods=( "zmax" "middle" )
 # loop through all input directories
 for well_fov in "${well_fovs[@]}"; do
     well_fov=${well_fov%*/}
@@ -29,7 +20,7 @@ for well_fov in "${well_fovs[@]}"; do
         --nodes=1 \
         --ntasks=1 \
         --account=amc-general \
-        --parition=amilan \
+        --partition=amilan \
         --qos=normal \
         --time=00:10:00 \
         --output="segmentation_child_${patient}_${well_fov}-%j.out" \
@@ -38,8 +29,5 @@ for well_fov in "${well_fovs[@]}"; do
         "$well_fov"
 done
 
-cd ../ || exit
-
-conda deactivate
 
 echo "Cell segmentation preprocessing completed successfully."
