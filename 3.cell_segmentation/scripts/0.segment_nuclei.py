6cba43491b4f861e990b3fbb4c48e89b75eddd30
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2025-07-02 10:49:41 -0600

    Update 3.cell_segmentation/scripts/0.segment_nuclei.py
    Co-authored-by: Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>

--- 3.cell_segmentation/scripts/0.segment_nuclei.py
+++ 3.cell_segmentation/scripts/0.segment_nuclei.py
@@ -33,7 +33,7 @@ except NameError:
 
 # ## parse args and set paths
 
-# If if a notebook run the hardcoded paths.
+# If as a notebook, then it will run the hardcoded paths.
 # However, if this is run as a script, the paths are set by the parsed arguments.
 
 # In[ ]:
